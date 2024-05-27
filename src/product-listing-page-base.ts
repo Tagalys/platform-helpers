2fb4aa206f4c49a8c5f4bc991bfe6abcbb525c73
Sudarshan <sudharshan@tagalys.com>
2024-05-27 16:43:06 +0530

    moved common logic to plp base

---
+++ src/product-listing-page-base.ts
@@ -0,0 +1,148 @@
+import { API_VERSION } from './lib/common';
+import GraphqlQueries from './lib/graphql-queries'
+import ShopifyAPI from './lib/shopifyApi'
+import GraphqlResponseFormatter from './lib/grapqhl-to-common-response-formatter';
+import globalContext from './lib/global-context';
+
+class Base{
+  public requestState
+  public responseState
+  public queries
+  public graphqlResponseFormatter;
+
+  constructor(requestState, responseState) {
+    this.requestState = requestState;
+    this.responseState = responseState
+    this.queries = new GraphqlQueries()
+    this.graphqlResponseFormatter = new GraphqlResponseFormatter()
+  }
+
+  apiClient() {
+    return new ShopifyAPI()
+  }
+
+  getPaginationVariables() {
+    let paginationVariables = {}
+    if (this.requestState.endCursor) {
+      paginationVariables['after'] = this.requestState.endCursor
+      paginationVariables['first'] = this.requestState.perPage
+    }
+    if (this.requestState.startCursor) {
+      paginationVariables['before'] = this.requestState.startCursor
+      paginationVariables['last'] = this.requestState.perPage
+    }
+    if (!this.requestState.startCursor && !this.requestState.endCursor) {
+      paginationVariables['first'] = this.requestState.perPage
+    }
+    return paginationVariables
+  }
+
+  getFilterVariables() {
+    let filterVariables = {}
+    if (Object.keys(this.requestState.filters).length) {
+      let filtersToApply = []
+      for (const [_, filterValues] of Object.entries(this.requestState.filters)) {
+        const values: any = filterValues
+        if (filterValues.hasOwnProperty("selected_min")) {
+          filtersToApply.push({
+            price: {
+              min: parseFloat(filterValues['selected_min']),
+              max: parseFloat(filterValues['selected_max'])
+            }
+          })
+        } else {
+          values.forEach((filterValue) => {
+            if (this.responseState.filter_inputs && this.responseState.filter_inputs[filterValue]) {
+              const selectedFilterValue = this.responseState.filter_inputs[filterValue]
+              filtersToApply.push(JSON.parse(selectedFilterValue.input))
+            }
+          })
+        }
+      }
+      filterVariables['productFilters'] = filtersToApply
+    }
+    return filterVariables
+  }
+
+  getMetafieldVariables() {
+    if (!globalContext.shopifyConfiguration.hasMetafields()) {
+      return {
+        product_metafields: [],
+      }
+    }
+    const metafieldsToQuery = globalContext.shopifyConfiguration.getMetafields()
+    return {
+      product_metafields: (metafieldsToQuery.products || []),
+    }
+  }
+
+  getSortOptions(requestOptions, defaultSortOptions) {
+    let sortOptions = (requestOptions.params.sort_options || defaultSortOptions )
+    sortOptions.forEach((sortOption: any) => {
+      if (sortOption.id === this.requestState.sort) {
+        sortOption.selected = true
+      }else{
+        sortOption.selected = false
+      }
+    })
+
+    // this logic can be improved, by defining a 'default' key
+    if(this.requestState.sort === null && sortOptions.length > 0){
+      sortOptions[0].selected = true
+    }
+    return sortOptions
+  }
+
+  getQuery() {}
+  getQueryVariables() { }
+  formatResponse(requestOptions, shopifyResponse) { }
+  getDataForInitialRequest(requestOptions) { }
+
+  getFilterData(response) {
+    const filterInputs = GraphqlResponseFormatter.getFilterInputs(response.search.filters)
+    const rangeFilter = response.search.filters.find((filter) => filter.type === "PRICE_RANGE")
+    let price_ranges = {}
+    if (rangeFilter) {
+      price_ranges = JSON.parse(rangeFilter.values[0].input).price
+    }
+
+    let filtersForRequestParams = {}
+    for (const [filterId, appliedFilterValues] of Object.entries(this.requestState.filters)) {
+      // checkbox filter
+      const appliedFilter = response.search.filters.find((filter) => filter.id === filterId)
+      if (Array.isArray(appliedFilterValues)) {
+        let formattedFilterValues = []
+        appliedFilterValues.forEach((filterLabel) => {
+          if (appliedFilter.type === "LIST" || appliedFilter.type === "BOOLEAN") {
+            appliedFilter.values.forEach((filterValue) => {
+              if (filterLabel === filterValue.label) {
+                formattedFilterValues.push(filterValue.id)
+              }
+            })
+          }
+        })
+        filtersForRequestParams[filterId] = formattedFilterValues
+      } else {
+        filtersForRequestParams[filterId] = appliedFilterValues
+      }
+    }
+
+    return {
+      filtersForRequestParams: filtersForRequestParams,
+      filter_inputs: filterInputs,
+      price_ranges: price_ranges
+    }
+  }
+
+  helpersToExpose() {
+    return {
+      getQuery: () => this.getQuery(),
+      getQueryVariables: () => this.getQueryVariables(),
+      formatResponse: (requestOptions, shopifyResponse) => this.formatResponse(requestOptions, shopifyResponse),
+      getFilterInputs: (filtersFromResponse) => GraphqlResponseFormatter.getFilterInputs(filtersFromResponse),
+      getDataForInitialRequest: (requestOptions) => this.getDataForInitialRequest(requestOptions),
+    }
+  }
+}
+
+export default Base
--- src/search.ts
+++ src/search.ts
@@ -1,8 +1,6 @@
-import { API_VERSION } from './lib/common';
-import GraphqlQueries from './lib/graphql-queries'
-import ShopifyAPI from './lib/shopifyApi'
 import GraphqlResponseFormatter from './lib/grapqhl-to-common-response-formatter';
 import globalContext from './lib/global-context';
+import Base from "./product-listing-page-base"
 
 const DEFAULT_SORT_OPTIONS = [
   {
@@ -19,102 +17,7 @@ const DEFAULT_SORT_OPTIONS = [
   }
 ]
 
-class Search {
-  private requestState
-  private responseState
-  private queries
-  private graphqlResponseFormatter;
-
-  constructor(requestState, responseState) {
-    this.requestState = requestState;
-    this.responseState = responseState
-    this.queries = new GraphqlQueries()
-    this.graphqlResponseFormatter = new GraphqlResponseFormatter()
-  }
-
-  getSortVariables() {
-    // for sort option id, refer DEFAULT_SORT_OPTIONS
-    const sortOptionIdToSortKeyMap = {
-      'relevance': {
-        sortKey: 'RELEVANCE',
-        reverse: false,
-      },
-      'price-desc': {
-        sortKey: 'PRICE',
-        reverse: true,
-      },
-      'price-asc': {
-        sortKey: 'PRICE',
-        reverse: false
-      }
-    }
-    return sortOptionIdToSortKeyMap[this.requestState.sort]
-  }
-
-  getPaginationVariables() {
-    let paginationVariables = {}
-    if (this.requestState.endCursor) {
-      paginationVariables['after'] = this.requestState.endCursor
-      paginationVariables['first'] = this.requestState.perPage
-    }
-    if (this.requestState.startCursor) {
-      paginationVariables['before'] = this.requestState.startCursor
-      paginationVariables['last'] = this.requestState.perPage
-    }
-    if (!this.requestState.startCursor && !this.requestState.endCursor) {
-      paginationVariables['first'] = this.requestState.perPage
-    }
-    return paginationVariables
-  }
-
-  getFilterVariables() {
-    let filterVariables = {}
-    if (Object.keys(this.requestState.filters).length) {
-      let filtersToApply = []
-      for (const [_, filterValues] of Object.entries(this.requestState.filters)) {
-        const values: any = filterValues
-        if (filterValues.hasOwnProperty("selected_min")) {
-          filtersToApply.push({
-            price: {
-              min: parseFloat(filterValues['selected_min']),
-              max: parseFloat(filterValues['selected_max'])
-            }
-          })
-        } else {
-          values.forEach((filterValue) => {
-            if (this.responseState.filter_inputs && this.responseState.filter_inputs[filterValue]) {
-              const selectedFilterValue = this.responseState.filter_inputs[filterValue]
-              filtersToApply.push(JSON.parse(selectedFilterValue.input))
-            }
-          })
-        }
-      }
-      filterVariables['productFilters'] = filtersToApply
-    }
-    return filterVariables
-  }
-
-  getMetafieldVariables() {
-    if (!globalContext.shopifyConfiguration.hasMetafields()) {
-      return {
-        product_metafields: [],
-      }
-    }
-    const metafieldsToQuery = globalContext.shopifyConfiguration.getMetafields()
-    return {
-      product_metafields: (metafieldsToQuery.products || []),
-    }
-  }
-
-  getQueryVariables() {
-    return {
-      query: `${this.requestState.query}`,
-      ...this.getSortVariables(),
-      ...this.getPaginationVariables(),
-      ...this.getFilterVariables(),
-      ...this.getMetafieldVariables()
-    }
-  }
+class Search extends Base{
 
   getQuery() {
     return `
@@ -132,7 +35,17 @@ class Search {
           country: ${globalContext.configuration.getCountryCode()},
           language: ${globalContext.configuration.getLanguageCode()}
         ) {
-        search(query: $query, first: $first, last: $last, after: $after, before: $before,  reverse: $reverse, sortKey: $sortKey, productFilters: $productFilters){
+        search(
+          query: $query,
+          first: $first,
+          last: $last,
+          after: $after,
+          before: $before,
+          reverse: $reverse,
+          sortKey: $sortKey,
+          productFilters: $productFilters
+        ){
+          query
           edges{
             node{
               ... on Product{
@@ -140,7 +53,7 @@ class Search {
               }
             }
           }
-          productFilters {
+          filters: productFilters {
             id
             label
             type
@@ -162,13 +75,42 @@ class Search {
     `
   }
 
+  getQueryVariables() {
+    return {
+      query: `${this.requestState.query}`,
+      ...this.getSortVariables(),
+      ...this.getPaginationVariables(),
+      ...this.getFilterVariables(),
+      ...this.getMetafieldVariables()
+    }
+  }
+
+  getSortVariables() {
+    // for sort option id, refer DEFAULT_SORT_OPTIONS
+    const sortOptionIdToSortKeyMap = {
+      'relevance': {
+        sortKey: 'RELEVANCE',
+        reverse: false,
+      },
+      'price-desc': {
+        sortKey: 'PRICE',
+        reverse: true,
+      },
+      'price-asc': {
+        sortKey: 'PRICE',
+        reverse: false
+      }
+    }
+    return sortOptionIdToSortKeyMap[this.requestState.sort]
+  }
+
   static getFilterInputsQuery() {
     return `
     query Search(
       $query: String!,
     ) {
       search(query: $query, first: 1){
-        productFilters {
+        filters: productFilters {
           id
           label
           type
@@ -184,47 +126,8 @@ class Search {
     `
   }
 
-  getRequestOptions() {
-    return {
-      path: "graphql.json",
-      apiVersion: API_VERSION
-    }
-  }
-
-  getSortOptions(requestOptions) {
-    let sortOptions = (requestOptions.params.sort_options || DEFAULT_SORT_OPTIONS )
-    sortOptions.forEach((sortOption: any) => {
-      if (sortOption.id === this.requestState.sort) {
-        sortOption.selected = true
-      }else{
-        sortOption.selected = false
-      }
-    })
-
-    // this logic can be improved, by defining a 'default' key
-    if(this.requestState.sort === null && sortOptions.length > 0){
-      sortOptions[0].selected = true
-    }
-    return sortOptions
-  }
-
-  formatResponse(requestOptions, shopifyResponse) {
-    // handle empty node: {}
-    return {
-      products: shopifyResponse.search.edges.map((productEdge) => this.graphqlResponseFormatter.formatProduct(productEdge.node)),
-      filters: this.graphqlResponseFormatter.formatFilters(shopifyResponse.search.productFilters, this.requestState.filters, this.responseState.price_ranges),
-      sort_options: this.getSortOptions(requestOptions),
-      page_info: shopifyResponse.search.pageInfo,
-      filter_inputs: GraphqlResponseFormatter.getFilterInputs(shopifyResponse.search.productFilters)
-    }
-  }
-
-  apiClient() {
-    return new ShopifyAPI()
-  }
-
   async getDataForInitialRequest(requestOptions) {
-    const filterInputsQuery = Search.getFilterInputsQuery()
+    const filterInputsQuery = Search.getFilterInputsQuery();
     const response = await this.apiClient().call("POST", "graphql.json", {
       params: JSON.stringify({
         query: filterInputsQuery,
@@ -233,49 +136,18 @@ class Search {
         }
       })
     })
-    const filterInputs = GraphqlResponseFormatter.getFilterInputs(response.search.productFilters)
-    const rangeFilter = response.search.productFilters.find((filter) => filter.type === "PRICE_RANGE")
-    let price_ranges = {}
-    if (rangeFilter) {
-      price_ranges = JSON.parse(rangeFilter.values[0].input).price
-    }
-
-
-    let filtersForRequestParams = {}
-    for (const [filterId, appliedFilterValues] of Object.entries(this.requestState.filters)) {
-      // checkbox filter
-      const appliedFilter = response.search.productFilters.find((filter) => filter.id === filterId)
-      if (Array.isArray(appliedFilterValues)) {
-        let formattedFilterValues = []
-        appliedFilterValues.forEach((filterLabel) => {
-          if (appliedFilter.type === "LIST" || appliedFilter.type === "BOOLEAN") {
-            appliedFilter.values.forEach((filterValue) => {
-              if (filterLabel === filterValue.label) {
-                formattedFilterValues.push(filterValue.id)
-              }
-            })
-          }
-        })
-        filtersForRequestParams[filterId] = formattedFilterValues
-      } else {
-        filtersForRequestParams[filterId] = appliedFilterValues
-      }
-    }
-
-    return {
-      filtersForRequestParams: filtersForRequestParams,
-      filter_inputs: filterInputs,
-      price_ranges: price_ranges
-    }
+    return this.getFilterData(response.search.filters)
   }
 
-  helpersToExpose() {
+  formatResponse(requestOptions, shopifyResponse) {
+    // handle empty node: {}
     return {
-      getQuery: () => this.getQuery(),
-      getQueryVariables: () => this.getQueryVariables(),
-      formatResponse: (requestOptions, shopifyResponse) => this.formatResponse(requestOptions, shopifyResponse),
-      getFilterInputs: (filtersFromResponse) => GraphqlResponseFormatter.getFilterInputs(filtersFromResponse),
-      getDataForInitialRequest: (requestOptions) => this.getDataForInitialRequest(requestOptions),
+      query: shopifyResponse.search.query,
+      products: shopifyResponse.search.edges.map((productEdge) => this.graphqlResponseFormatter.formatProduct(productEdge.node)),
+      filters: this.graphqlResponseFormatter.formatFilters(shopifyResponse.search.filters, this.requestState.filters, this.responseState.price_ranges),
+      sort_options: this.getSortOptions(requestOptions, DEFAULT_SORT_OPTIONS),
+      page_info: shopifyResponse.search.pageInfo,
+      filter_inputs: GraphqlResponseFormatter.getFilterInputs(shopifyResponse.search.filters)
     }
   }
 
