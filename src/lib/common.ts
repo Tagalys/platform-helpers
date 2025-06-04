448aabd71ff89afe5f890838f28a6fd8d446a1ae
Sudarshan <sudharshan@tagalys.com>
2025-06-05 00:14:41 +0530

    latest API version

--- src/lib/common.ts
+++ src/lib/common.ts
@@ -93,7 +93,7 @@ export const getPriceDetails = (product) => {
   }
 }
 
-export const API_VERSION = '2025-01'
+export const API_VERSION = '2025-04'
 
 export const METAFIELD_TYPES = {
   LIST_PRODUCT_REFERENCE: 'list.product_reference',
